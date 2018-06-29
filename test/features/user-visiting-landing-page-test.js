328c47f95a3abb2a7eea989a0114473ea647d208
jp <jp.san.gabriel01@gmail.com>
2018-06-29 09:54:59 +0800

    Add test for user visiting landing page without existing videos

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,13 @@
 const {assert} = require('chai');
+
+describe('User visits landing page', () => {
+  describe('without existing videos', () => {
+    it('starts blank', () => {
+      // Exercise
+      browser.url('/');
+
+      // Verify
+      assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+});
