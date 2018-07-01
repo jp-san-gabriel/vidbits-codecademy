8fa377981cea9b25d632d705846f10a5d304917e
jp <jp.san.gabriel01@gmail.com>
2018-07-01 21:32:24 +0800

    Add test for user posting a video and seeing it rendered.

---
+++ test/features/user-visiting-save-video-page-test.js
@@ -0,0 +1,24 @@
+const {assert} = require('chai');
+
+describe('User visits "Save Video" page', () => {
+  describe('posts a new item', () => {
+    it('and is rendered', () => {
+      // Setup
+      const videoToSave = {
+        title: 'Shampoo Prank',
+        description: 'cold water edition',
+        videoUrl: 'https://www.youtube.com/watch?v=oVm7FkQI4BM'
+      }
+      // Exercise
+      browser.url('/videos/create.html');
+      browser.setValue('#title-input', videoToSave.title);
+      browser.setValue('#description-input', videoToSave.description);
+      browser.setValue('#videoUrl-input', videoToSave.videoUrl);
+      browser.click('#submit-button');
+
+      // Verify
+      assert.include(browser.getText('body'), videoToSave.title);
+      assert.include(browser.getText('body'), videoToSave.description);
+    });
+  });
+});
