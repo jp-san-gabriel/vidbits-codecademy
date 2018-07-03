27a94a2402eeef7f47d5b0799d17893ae4ba6118
jp <jp.san.gabriel01@gmail.com>
2018-07-03 09:15:35 +0800

    Add test: Post to '/videos' must save a Video

--- test/routes/save-video-test.js
+++ test/routes/save-video-test.js
@@ -1,14 +1,36 @@
 const {assert} = require('chai');
 const request = require('supertest');
 const app = require('../../app');
+const Video = require('../../models/video');
 
 describe('Server path: /videos', () => {
-  describe('POST', async() => {
+  describe('POST', () => {
     it('responds 201 status for new video creation', async () => {
       const response = await request(app)
         .post('/videos')
         .type('form')
       assert.equal(response.status, 201);
+    });
+  });
+
+  describe('POST', () => {
+    it('saves a video', async () => {
+      // Setup
+      const videoToSave = {
+        title: 'Shampoo Prank',
+        description: 'cold water edition'
+      };
+
+      // Exercise
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(videoToSave);
+
+      // Verify
+      const savedVideo = await Video.findOne({});
+      assert.equal(savedVideo.title, videoToSave.title);
+      assert.equal(savedVideo.description, videoToSave.description);
     });
   });
 });
