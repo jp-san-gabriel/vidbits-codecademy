f4cbf82c42f0b9d40f63adc6e74e24c16a23ca64
jp <jp.san.gabriel01@gmail.com>
2018-07-03 15:18:58 +0800

    Modify response to post request to '/videos'

--- routes/videos.js
+++ routes/videos.js
@@ -4,7 +4,10 @@ const Video = require('../models/video');
 router.post('/videos', async (req, res) => {
   const {title, description} = req.body;
   await Video.create({title, description});
-  res.sendStatus(201);
+  res.status(201).send(`
+    <h1>${title}</h1>
+    <p>${description}</p>
+  `);
 });
 
 module.exports = router;
