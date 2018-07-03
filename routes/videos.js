a37b0e8f0e3431e8fbd779518b4490fcaa51b6b8
jp <jp.san.gabriel01@gmail.com>
2018-07-03 10:10:47 +0800

    Make post route '/videos' save Video to database

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const Video = require('../models/video');
 
-router.post('/videos', (req, res) => {
+router.post('/videos', async (req, res) => {
+  const {title, description} = req.body;
+  await Video.create({title, description});
   res.sendStatus(201);
 });
 
