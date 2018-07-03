fcb3896ce9e976078b85e8500a58d40b5e7900e2
jp <jp.san.gabriel01@gmail.com>
2018-07-03 08:51:46 +0800

    Make test more expressive

--- test/routes/save-video-test.js
+++ test/routes/save-video-test.js
@@ -3,10 +3,12 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('Server path: /videos', () => {
-  it('POST', async () => {
-    const response = await request(app)
-      .post('/videos')
-      .type('form')
-    assert.equal(response.status, 201);
+  describe('POST', async() => {
+    it('responds 201 status for new video creation', async () => {
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+      assert.equal(response.status, 201);
+    });
   });
 });
