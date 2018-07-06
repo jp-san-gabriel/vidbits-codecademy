6c8da396bbe0a5f79e021477848e58c97096fb89
jp <jp.san.gabriel01@gmail.com>
2018-07-06 09:54:11 +0800

    Make POST '/videos' render error message when video title is empty

--- routes/videos.js
+++ routes/videos.js
@@ -11,7 +11,8 @@ router.post('/videos', async (req, res) => {
     const video = await Video.create({title, description});
     res.status(201).render('videos/show', {video});
   } else {
-    res.status(400).render('videos/create');
+    const error = 'Title is required';
+    res.status(400).render('videos/create', {error});
   }
 });
 
