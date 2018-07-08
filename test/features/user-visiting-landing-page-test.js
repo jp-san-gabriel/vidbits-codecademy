9c475e273c20cba44e3488b6f277708cf7af0444
jp <jp.san.gabriel01@gmail.com>
2018-07-08 20:51:41 +0800

    Add assertion to check that video url is rendered in an iframe

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -10,6 +10,29 @@ describe('User visits landing page', () => {
       assert.equal(browser.getText('#videos-container'), '');
     });
   });
+
+  describe('with existing videos', () => {
+    it('renders it in the list', () => {
+      // Setup - save a video
+      const videoToSave = {
+        title: 'Shampoo Prank',
+        description: 'cold water edition',
+        videoUrl: 'https://www.youtube.com/watch?v=oVm7FkQI4BM'
+      }
+
+      browser.url('/videos/create.html');
+      browser.setValue('#title-input', videoToSave.title);
+      browser.setValue('#videoUrl-input', videoToSave.videoUrl);
+      browser.click('#submit-button');
+
+      // Exercise
+      browser.url('/');
+
+      // Verify
+      assert.equal(browser.getAttribute('iframe', 'src'), videoToSave.videoUrl);
+    });
+  });
+
   describe('can navigate', () => {
     it('to save video page', () => {
       // Setup
