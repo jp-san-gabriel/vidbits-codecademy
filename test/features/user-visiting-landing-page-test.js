2e5dae06f7abe2429f6d4acae3c9ce3aed81089f
jp <jp.san.gabriel01@gmail.com>
2018-07-09 09:48:20 +0800

    Refactor: move code for submitting a new video to a helper function in feature tests

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -4,6 +4,21 @@ const generateRandomUrl = (domain) => {
     return `http://${domain}/${Math.random()}`;
 };
 
+// This function prevents runtime error in case value is undefined
+const setElementValue = (selector, value) => {
+    if(value) {
+      browser.setValue(selector, value);
+    }
+};
+
+const submitVideo = (video) => {
+  browser.url('/videos/create.html');
+  setElementValue('#title-input', video.title);
+  setElementValue('#description-input', video.description);
+  setElementValue('#videoUrl-input', video.videoUrl);
+  browser.click('#submit-button');
+};
+
 describe('User visits landing page', () => {
   describe('without existing videos', () => {
     it('starts blank', () => {
@@ -24,10 +39,7 @@ describe('User visits landing page', () => {
         videoUrl: generateRandomUrl('youtube.com')
       }
 
-      browser.url('/videos/create.html');
-      browser.setValue('#title-input', videoToSave.title);
-      browser.setValue('#videoUrl-input', videoToSave.videoUrl);
-      browser.click('#submit-button');
+      submitVideo(videoToSave);
 
       // Exercise
       browser.url('/');
@@ -44,11 +56,7 @@ describe('User visits landing page', () => {
         videoUrl: generateRandomUrl('youtube.com')
       }
 
-      browser.url('/videos/create.html');
-      browser.setValue('#title-input', videoToSave.title);
-      browser.setValue('#description-input', videoToSave.description);
-      browser.setValue('#videoUrl-input', videoToSave.videoUrl);
-      browser.click('#submit-button');
+      submitVideo(videoToSave);
 
       // Exercise
       browser.url('/');
