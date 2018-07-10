64d88e8f2f5b2f1a8f3eba4875e5a60337598676
jp <jp.san.gabriel01@gmail.com>
2018-07-10 09:37:59 +0800

    Add test: POST '/videos/:id/updates' must render edit form for invalid inputs

--- test/routes/videos-test.js
+++ test/routes/videos-test.js
@@ -282,27 +282,50 @@ describe("Server path: /videos/:id/updates", () => {
       assert.equal(response.headers.location, `/videos/${video._id}`);
     });
 
-    it('does not save an invalid record', async () => {
-      // Setup - save the video to database
-      const video = await Video.create({
-        title: 'Shampoo Prank 890111',
-        description: 'Cold water edition',
-        videoUrl: 'youtube.com/embed/jfldslie'
+    describe('when input is invalid', () => {
+      it('does not save the record', async () => {
+        // Setup - save the video to database
+        const video = await Video.create({
+          title: 'Shampoo Prank 890111',
+          description: 'Cold water edition',
+          videoUrl: 'youtube.com/embed/jfldslie'
+        });
+
+        // Exercise - set the video title to blank then submit the video
+        video.title = '';
+        const response = await request(app)
+          .post(`/videos/${video._id}/updates`)
+          .type('form')
+          .send(video.toObject());
+
+        // Verify - check that the title still is not blank to see that it was not updated
+        const updatedVideo = await Video.findById(video._id);
+        assert.notEqual(updatedVideo.title, video.title);
+
+        // Verify - check that it responds with a 400
+        assert.equal(response.status, 400);
       });
 
-      // Exercise - set the video title to blank then submit the video
-      video.title = '';
-      const response = await request(app)
-        .post(`/videos/${video._id}/updates`)
-        .type('form')
-        .send(video.toObject());
+      it('renders the edit form', async () => {
+        // Setup - save the video to database
+        const video = await Video.create({
+          title: 'Shampoo Prank 890111',
+          description: 'Cold water edition',
+          videoUrl: 'youtube.com/embed/jfldslie'
+        });
 
-      // Verify - check that the title still is not blank to see that it was not updated
-      const updatedVideo = await Video.findById(video._id);
-      assert.notEqual(updatedVideo.title, video.title);
+        // Exercise - set the video title to blank then submit the video
+        video.title = '';
+        const response = await request(app)
+          .post(`/videos/${video._id}/updates`)
+          .type('form')
+          .send(video.toObject());
 
-      // Verify - check that it responds with a 400
-      assert.equal(response.status, 400);
+        // Verify that input fields contain the values of the submitted video
+        assert.equal(getElementFromHtml(response.text, 'form input[id="title-input"]').value, video.title);
+        assert.equal(getElementFromHtml(response.text, 'form textarea[id="description-input"]').value, video.description);
+        assert.equal(getElementFromHtml(response.text, 'form input[id="videoUrl-input"]').value, video.videoUrl);
+      });
     });
   });
 });
