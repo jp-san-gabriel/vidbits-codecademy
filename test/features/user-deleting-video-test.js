00b804d9450aa7c2fd84637644a89657cbc79f15
jp <jp.san.gabriel01@gmail.com>
2018-07-12 11:35:00 +0800

    Refactor: Import 'submitVideo' helper function in 'user-deleting-video-test.js'

--- test/features/user-deleting-video-test.js
+++ test/features/user-deleting-video-test.js
@@ -1,17 +1,13 @@
 const {assert} = require('chai');
-const {buildVideoObject} = require('../test-utilities');
+const {buildVideoObject, submitVideo} = require('../test-utilities');
 
 describe('User deleting video', () => {
 
   it('removes the Video from the list', () => {
     // Setup - submit a video
     const videoToSave = buildVideoObject();
-    
-    browser.url('/videos/create.html');
-    browser.setValue('#title-input', videoToSave.title);
-    browser.setValue('#description-input', videoToSave.description);
-    browser.setValue('#videoUrl-input', videoToSave.videoUrl);
-    browser.click('#submit-button');
+
+    submitVideo(videoToSave);
 
     // Exercise - click the delete button from the video's show page
     browser.click('#delete');
