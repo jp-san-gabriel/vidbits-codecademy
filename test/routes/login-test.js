41f77e5ed9f39904c88104bb8edcbd4ef2aef5db
jp <jp.san.gabriel01@gmail.com>
2018-07-16 13:43:43 +0800

    Add test: POST '/login' must redirect to landing page

--- test/routes/login-test.js
+++ test/routes/login-test.js
@@ -2,8 +2,15 @@ const {assert} = require('chai');
 const request = require('supertest');
 const app = require('../../app');
 const {getElementFromHtml} = require('../test-utilities');
+const {connectDatabase, disconnectDatabase} = require('../database-utilities');
 
 describe('Server path: /login', () => {
+  // Setup Phase
+  beforeEach(connectDatabase);
+
+  // Teardown Phase
+  afterEach(disconnectDatabase);
+
   describe('GET', () => {
     it('renders empty login form', async () => {
       // setup
@@ -22,6 +29,20 @@ describe('Server path: /login', () => {
       // Setup
       const user = 'admin';
       const password = 'password';
+      // Exercise - send the username and password
+      const response = await request(app)
+              .post('/login')
+              .type('form')
+              .send({user, password});
+
+      // Verify - that the response contains the username
+      assert.include(response.text, user);
+    });
+
+    it('redirects to landing page', async () => {
+      // Setup
+      const user = 'admin';
+      const password = 'password';
 
       // Exercise - send the username and password
       const response = await request(app)
@@ -29,8 +50,9 @@ describe('Server path: /login', () => {
         .type('form')
         .send({user, password});
 
-      // Verify - that the response contains the username
-      assert.include(response.text, user);
+      // Verify
+      assert.equal(response.status, 302);
+      assert.equal(response.headers.location, '/');
     });
   });
 });
