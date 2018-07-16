e8730f7cac86d8e3c67b1667788a49f6da1b1634
jp <jp.san.gabriel01@gmail.com>
2018-07-16 16:47:47 +0800

    Refactor feature test: Remove 'log out' test since it is already coverd in server level tests

--- test/features/user-logs-in-test.js
+++ test/features/user-logs-in-test.js
@@ -15,20 +15,4 @@ describe('User logs in', () => {
     // Verify - that the page does not include the 'Log in' link
     assert.notInclude(browser.getText('body'), 'Log in');
   });
-
-  it('and can log out', () => {
-    // Setup
-    const credentials = getValidCredentials();
-
-    // Exercise
-    browser.url('/login');
-    browser.setValue('form input[name="user"]', credentials.user);
-    browser.setValue('form input[name="password"]', credentials.password);
-    browser.click('#log-in');
-    browser.click('a[href="/logout"]');
-
-    // Verify - that the page includes the 'log in' link and does not contain the 'log out'
-    assert.notInclude(browser.getText('body'), 'Log out');
-    assert.include(browser.getText('body'), 'Log in');
-  });
 });
